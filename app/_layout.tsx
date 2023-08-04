cb3ba5c0384263cdc7bb1ec1acd52391e78558ad
Andrew Pethoud <andrewpethoud@gmail.com>
2023-08-04 16:47:27 -0700

    chore(tailwindcss): setup tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Slot } from 'expo-router';
 import React from 'react';
 
+import "../global.css";
+
 const RootLayout: React.FC = () => {
   return (
-    <>
+    <View className="m-8">
       <Text>RootLayout</Text>
       <Slot />
-    </>
+    </View>
   )
 }
 
