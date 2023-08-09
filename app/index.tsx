e89a42066c1cd6922fb62aec03ac8aa263433a24
Andrew Pethoud <andrewpethoud@gmail.com>
2023-08-09 11:43:42 -0700

    chore(Header): remove obsolete header component

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import Button from '../components/Button';
 export default function Welcome() {
   return (
     <View className="flex-1 items-center justify-around p-4 w-full">
-      <Header displaySerif huge centered>Find a great park for your kids</Header>
+      <Text displaySerif huge centered>Find a great park for your kids</Text>
       <View className="h-1/2 w-full bg-neutral-200" />
       <Text large centered>PlayPath helps you find great parks to visit along your route while traveling with small kids.</Text>
       <Button text="Get Started" navTo="search-criteria" />
