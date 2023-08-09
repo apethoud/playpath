8f8be4663baee394c409144e134347f1eb40e92f
Andrew Pethoud <andrewpethoud@gmail.com>
2023-08-09 13:28:54 -0700

    refactor(Button): move specific implementation of onPress logic out of Button

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,20 @@
 import { View } from 'react-native';
 import Text from '../components/Text';
 import Button from '../components/Button';
+import { useRouter } from 'expo-router';
 
 export default function Welcome() {
+  const navigation = useRouter()
+
+  const navigateToSearchCriteriaPage = () => {
+    navigation.push("search-criteria")
+  }
   return (
     <View className="flex-1 items-center justify-around p-4 w-full">
       <Text displaySerif huge centered>Find a great park for your kids</Text>
       <View className="h-1/2 w-full bg-neutral-200" />
       <Text large centered>PlayPath helps you find great parks to visit along your route while traveling with small kids.</Text>
-      <Button text="Get Started" navTo="search-criteria" />
+      <Button text="Get Started" onPress={navigateToSearchCriteriaPage} />
     </View>
   );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -2,13 +2,9 @@ import { Pressable, View } from "react-native";
 import Text from "./Text";
 import { useRouter } from "expo-router";
 
-export default function Button({ navTo, secondary, text }: { navTo: string, secondary?: boolean, text: string }) {
-  const navigation = useRouter()
-
+export default function Button({ onPress, secondary, text }: { onPress: () => void, secondary?: boolean, text: string }) {
   return (
-    <Pressable onPress={() => {
-      navigation.push(navTo)
-    }}>
+    <Pressable onPress={onPress}>
       <View className="px-2 py-1 bg-neutral-600 rounded-lg">
         <Text light={!secondary}>{text}</Text>
       </View>
