cd0b78ceb91e9be1bf222a9f4e2bf1ad9da4f28e
Andrew Pethoud <andrewpethoud@gmail.com>
2023-08-10 14:17:53 -0700

    chore(TextInput): remove unused generic

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -2,7 +2,7 @@ import { Dispatch, SetStateAction } from "react";
 import { Control, Controller } from "react-hook-form";
 import { TextInput as RNTextInput } from "react-native";
 
-export default function TextInput<T>({ control, name }: { control: Control<{ destination: string }>, name: "destination" }) {
+export default function TextInput({ control, name }: { control: Control<{ destination: string }>, name: "destination" }) {
   return (
     <Controller
       control={control}
